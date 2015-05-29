418bc76e7fcbaa89d010dbe83d74d7088ee7f06d
Sven Liebig <liebigsv@gmail.com>
2015-05-29 16:36:46 +0200

    0.2.7

--- script.tamper.js
+++ script.tamper.js
@@ -6,7 +6,7 @@
 // @include      http://bs.to/
 // @include		 http://bs.to/home
 // @icon		 http://s.bs.to/favicon.ico
-// @version      0.2.6
+// @version      0.2.7
 // @grant        none
 // @updateURL	 https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/script.tamper.js
 // ==/UserScript==
