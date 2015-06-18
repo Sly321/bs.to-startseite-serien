aa26d3b70a872093d9e0ff3dc2f318b3667ea4fc
Sven Liebig <liebigsv@gmail.com>
2015-06-18 02:55:54 +0200

    Shingeki no Kiojin fix

--- anime-toplist.user.js
+++ anime-toplist.user.js
@@ -5,7 +5,7 @@
 // @description  Zeigt dir eine Anime Toplist auf der Startseite an 25/100 verlinkt.
 // @include      http://bs.to/
 // @icon		 http://s.bs.to/favicon.ico
-// @version      0.8
+// @version      0.8.1
 // @grant        none
 // @require		 https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
 // @updateURL	 https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/anime-toplist.user.js
