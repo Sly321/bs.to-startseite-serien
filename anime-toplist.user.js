781d2c03a565690cc768d28cc1c1e5565872f434
Sven Liebig <liebigsv@gmail.com>
2016-05-06 13:12:48 +0200

    added house of cards (again)

--- anime-toplist.user.js
+++ anime-toplist.user.js
@@ -5,7 +5,7 @@
 // @description  Zeigt dir eine Anime Toplist auf der Startseite an 25/100 verlinkt.
 // @include      https://bs.to/
 // @icon         https://s.bs.to/favicon.ico
-// @version      0.9.0.5
+// @version      0.9.0.6
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
 // @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/anime-toplist.user.js
@@ -41,6 +41,7 @@ var SerienTabContainerLinks = [new SerienTab("Serien", [["Agents-of-S-H-I-E-L-D"
                                                         ["Game of Thrones", "Game-of-Thrones"], 
                                                         ["Gotham", "Gotham"],
                                                         ["Hannibal", "Hannibal"], 
+                                                        ["House of Cards", "House-of-Cards-US"], 
                                                         ["iZombie", "iZombie"], 
                                                         ["Lucifer", "Lucifer"], 
                                                         ["Marvels Agent Carter", "Marvel-s-Agent-Carter"], 
