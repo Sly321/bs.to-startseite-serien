bc62ee6eee0a2adefafa601592f4406e2b37fbbd
Sven Liebig <liebigsv@gmail.com>
2016-05-11 22:01:52 +0200

    margins

--- anime-toplist.user.js
+++ anime-toplist.user.js
@@ -5,7 +5,7 @@
 // @description  Zeigt dir eine Anime Toplist auf der Startseite an 25/100 verlinkt.
 // @include      https://bs.to/
 // @icon         https://s.bs.to/favicon.ico
-// @version      0.9.2.6
+// @version      0.9.2.7
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
 // @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/anime-toplist.user.js
@@ -220,6 +220,7 @@ addGlobalStyle('#ui-accordion-accordionRechts-header-3 { padding: 5px 0px 5px 30
 addGlobalStyle('#ui-accordion-accordionLinks-header-4 { padding: 5px 0px 5px 30px; }');
 addGlobalStyle('#ui-accordion-accordionRechts-header-4 { padding: 5px 0px 5px 30px; }');
 addGlobalStyle('#ui-tabs-5 > div { padding: 15px; }');
+addGlobalStyle('#ui-tabs-5 > div >h3 { margin-top: 0; margin-bottom: 10px; }');
 addGlobalStyle('#ui-tabs-5 > div > input { margin-right: 15px; }');
 addGlobalStyle('.delbtn { color: white !important; float: right; font-size: 14px; border: none; background: #212121; border-radius: 100%; height: 19px; width: 20px; margin-top: 1px; padding: 0px 0px 0px 0px; z-index: 15; text-decoration: none; text-align: center; margin-left: 5px; }');
 
@@ -754,7 +755,7 @@ var lTitles = $("#column1 div h3 .header-title");
 var rTitles = $("#column2 div h3 .header-title");
 
 var preferencesTab = $('#ui-tabs-5');
-var input = "<div><h3>Check to Hide</h3><br>";
+var input = "<div><h3>Check to Hide</h3>";
 
 for (var x = 0; x < lTitles.length; x++) {
     input += '<input type="checkbox" name="' + lTitles[x].innerHTML + '" value="0"/>' + lTitles[x].innerHTML + "<br>";
