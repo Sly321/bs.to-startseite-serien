c2e14fce0c88e0ba15e63995402a46cec54e96cb
Sven Liebig <liebigsv@gmail.com>
2016-05-27 00:51:23 +0200

    current episode wird markiert, was beinbruch diese scheiße

--- bs.to/bs.to-serien-info-script.user.js
+++ bs.to/bs.to-serien-info-script.user.js
@@ -9,6 +9,11 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
 // @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/bs.to/bs-to-info-script.user.js
 // ==/UserScript==
+// 
+var curepisode = 0;
+var curstaffel = 0;
+var lEle;
+var rEle;
 
 var div = $(document.createElement("div"));
 var wrapper = $(document.createElement("div"));
@@ -51,7 +56,7 @@ wrapper.append(seasonTable);
 wrapper.append(closeA);
 $("body").append(div);
 
-var loadSeriesInfo = function(data, action) {
+var loadSeriesInfo = function(data, action, aLink) {
     switch(action) {
         case 'load-series':
             $("#infodiv").css("opacity", "1");
@@ -76,15 +81,21 @@ var loadSeriesInfo = function(data, action) {
             var episoden = data.episodes;
             var htmlString = '';
             for(var x = 0; x < episoden.length; x++) {
-                htmlString += '<tr style="background: rgb(13, 15, 18); border-bottom: 2px solid #2E2F31;">';
-                htmlString += '<td>' + episoden[x].episode_number + '</td>';
-                htmlString += '<td style="width: 400px">' + (episoden[x].name === '' ? '- keiner angegeben -' : episoden[x].name) + '</td>';
 
-                var dateStyle = "";
+                var namestyle = "";
+                if (data.season_number == $(aLink).attr("staffel") && episoden[x].episode_number == $(aLink).attr("episode")) {
+                    namestyle += "background: #3A3A3A;";
+                }
+
+                htmlString += '<tr style="background: rgb(13, 15, 18); border-bottom: 2px solid #2E2F31;">';
+                htmlString += '<td style="border-bottom-left-radius: 5px;border-top-left-radius: 5px;' + namestyle + '">' + episoden[x].episode_number + '</td>';
+                htmlString += '<td style="width: 400px;' + namestyle + '">' + (episoden[x].name === '' ? '- keiner angegeben -' : episoden[x].name) + '</td>';
+
+                var dateStyle = "border-top-right-radius: 5px;border-bottom-left-radius: 5px;" + namestyle;
                 var datestring = "";
                 if (episoden[x].air_date === null) {
                     datestring = "- unbekannt -";
-                    dateStyle = "float: right;";
+                    dateStyle += "text-align: right;";
                 } else {
                     var date = new Date(episoden[x].air_date);
                     var today = new Date();
@@ -92,11 +103,11 @@ var loadSeriesInfo = function(data, action) {
                     var mon = date.getMonth().toString().length == 1 ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
                     datestring += tag + "." + mon + "." + date.getFullYear();
                     if(date < today) {
-                        dateStyle = "float: right;";
+                        dateStyle += "text-align: right;";
                     } else if (date > today) {
-                        dateStyle = "float: right; color: #C2261A; font-weight: bolder;";
+                        dateStyle += "text-align: right; color: #C2261A; font-weight: bolder;";
                     } else {
-                        dateStyle = "float: right; color: #057729; font-weight: bolder;";
+                        dateStyle += "text-align: right; color: #057729; font-weight: bolder;";
                     }
                 }
 
@@ -113,7 +124,7 @@ var loadSeriesInfo = function(data, action) {
 var url = 'https://api.themoviedb.org/3/';
 var key = 'api_key=2e74839a423b1266f0ccf5043bade403';
 
-var getSerieById = function(id) {
+var getSerieById = function(id, aLink) {
     var mode = 'tv/' + id;
     $.ajax({
         type: 'GET',
@@ -125,7 +136,7 @@ var getSerieById = function(id) {
         success: function(json) {
             console.dir(json);
             loadSeriesInfo(json, 'load-series');
-            getSeasonById(id, json.number_of_seasons);
+            getSeasonById(id, json.number_of_seasons, aLink);
         },
         error: function(e) {
             console.log(e.message);
@@ -138,7 +149,7 @@ var resultSet = function(data) {
     var name   = data.original_name;
 };
 
-var getSeasonById = function(id, season) {
+var getSeasonById = function(id, season, aLink) {
     var mode = 'tv/' + id + '/season/' + season;
     $.ajax({
         type: 'GET',
@@ -149,7 +160,7 @@ var getSeasonById = function(id, season) {
         dataType: 'jsonp',
         success: function(json) {
             console.dir(json);
-            loadSeriesInfo(json, 'load-season');
+            loadSeriesInfo(json, 'load-season', aLink);
         },
         error: function(e) {
             console.log(e.message);
@@ -157,7 +168,7 @@ var getSeasonById = function(id, season) {
     });
 };
 
-var getSerieByName = function(name) {
+var getSerieByName = function(name, aLink) {
     var mode = 'search/tv?query=';
     $.ajax({
         type: 'GET',
@@ -167,16 +178,69 @@ var getSerieByName = function(name) {
         contentType: 'application/json',
         dataType: 'jsonp',
         success: function(json) {
-            getSerieById(json.results[0].id);
+            getSerieById(json.results[0].id, aLink);
         },
         error: function(e) {
             console.log(e.message);
         }
     });
 };
+
+var setFunctionLinks = function() {
+    console.log("lEle.length: " + lEle.length);
+    var a;
+    var serie_name = "";
+    for(var x = 0; x < lEle.length; x++) {
+       console.log("test3");
+       serie_name = (lEle[x].children[0].innerHTML);
+
+       curepisode = 0;
+       curstaffel = 0;
+       if(lEle[x].children[1] !== undefined) {
+            console.log("nicht undefined");
+          var lastseriestring = lEle[x].children[1].innerHTML;
+          var regexr = /[\w]*:\sS(\d)*\sE([\d]*)/;
+          var match = lastseriestring.match(regexr);
+          curstaffel = match[1];
+          curepisode = match[2];
+       }
+
+       new_a = $(document.createElement("a"));
+       new_a.html("🕐");
+       new_a.attr("id", serie_name);
+       new_a.css("cursor", "help");
+       new_a.css("margin-left", "5px");
+       new_a.css("font-size","16px");
+       new_a.attr("staffel", curstaffel);
+       new_a.attr("episode", curepisode);
+       new_a.on('click', function() {
+           console.log($(this).attr("id"));
+           aString = $(this).attr("id");
+           getSerieByName(aString, this);
+       });
+       $(lEle[x]).append(new_a);
+    }
+
+    for(var x = 0; x < rEle.length; x++) {
+       serie_name = (rEle[x].children[0].innerHTML);
+       new_a = $(document.createElement("a"));
+       new_a.html("🕐");
+       new_a.attr("id", serie_name);
+       new_a.css("cursor", "help");
+       new_a.css("margin-left", "5px");
+       new_a.css("font-size","16px");
+       new_a.on('click', function() {
+           console.log($(this).attr("id"));
+           aString = $(this).attr("id");
+           getSerieByName(aString);
+       });
+       $(rEle[x]).append(new_a);
+    }
+};
+
  $(document).ready(function() {
-    var lEle = $('#column1 > div > .ui-accordion-content > ul > li');
-    var rEle = $('#column2 > div > .ui-accordion-content > ul > li');
+    lEle = $('#column1 > div > .ui-accordion-content > ul > li');
+    rEle = $('#column2 > div > .ui-accordion-content > ul > li');
     if(rEle.length === 0 || lEle.length === 0) {
         var eleInt = setInterval(function() {
            lEle = $('#column1 > div > .ui-accordion-content > ul > li');
@@ -185,78 +249,10 @@ var getSerieByName = function(name) {
            console.log("interval3");
            if(rEle.length !== 0 && lEle.length !== 0) {
                clearInterval(eleInt);
-
-               console.log("lEle.length: " + lEle.length);
-               var a;
-               var serie_name = "";
-               for(var x = 0; x < lEle.length; x++) {
-                   console.log("test3");
-                   serie_name = (lEle[x].children[0].innerHTML);
-                   new_a = $(document.createElement("a"));
-                   new_a.html("🕐");
-                   new_a.attr("id", serie_name);
-                   new_a.css("cursor", "help");
-                   new_a.css("margin-left", "5px");
-                   new_a.css("font-size","16px");
-                   new_a.on('click', function() {
-                       console.log($(this).attr("id"));
-                       aString = $(this).attr("id");
-                       getSerieByName(aString);
-                   });
-                   $(lEle[x]).append(new_a);
-               }
-
-               for(var x = 0; x < rEle.length; x++) {
-                   serie_name = (rEle[x].children[0].innerHTML);
-                   new_a = $(document.createElement("a"));
-                   new_a.html("🕐");
-                   new_a.attr("id", serie_name);
-                   new_a.css("cursor", "help");
-                   new_a.css("margin-left", "5px");
-                   new_a.css("font-size","16px");
-                   new_a.on('click', function() {
-                       console.log($(this).attr("id"));
-                       aString = $(this).attr("id");
-                       getSerieByName(aString);
-                   });
-                   $(rEle[x]).append(new_a);
-               }
-           }
+               setFunctionLinks();
+            }
         }, 200);
     } else {
-        var a;
-        var serie_name = "";
-        for(var x = 0; x < lEle.length; x++) {
-            console.log("test3");
-            serie_name = (lEle[x].children[0].innerHTML);
-            new_a = $(document.createElement("a"));
-            new_a.html("🕐");
-            new_a.attr("id", serie_name);
-            new_a.css("cursor", "help");
-            new_a.css("margin-left", "5px");
-            new_a.css("font-size","16px");
-            new_a.on('click', function() {
-                console.log($(this).attr("id"));
-                aString = $(this).attr("id");
-                getSerieByName(aString);
-            });
-            $(lEle[x]).append(new_a);
-        }
-
-        for(var x = 0; x < rEle.length; x++) {
-            serie_name = (rEle[x].children[0].innerHTML);
-            new_a = $(document.createElement("a"));
-            new_a.html("🕐");
-            new_a.attr("id", serie_name);
-            new_a.css("cursor", "help");
-            new_a.css("margin-left", "5px");
-            new_a.css("font-size","16px");
-            new_a.on('click', function() {
-                console.log($(this).attr("id"));
-                aString = $(this).attr("id");
-                getSerieByName(aString);
-            });
-            $(rEle[x]).append(new_a);
-        }
-    }
+       setFunctionLinks();
+   }
 });
