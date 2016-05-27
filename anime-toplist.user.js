7b7b90ceb23f28af30fedf2a8730759faf83a3b0
Sven Liebig <liebigsv@gmail.com>
2016-05-27 02:29:28 +0200

    versionig

--- anime-toplist.user.js
+++ anime-toplist.user.js
@@ -1,11 +1,11 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         bs.to Startseiten script
 // @namespace    https://github.com/Sly321/bs.to-startseite-serien
 // @author       Sly321
 // @description  Zeigt dir deine Lieblingsserien auf der Startseiten an uvm.
 // @include      https://bs.to/
 // @icon         https://s.bs.to/favicon.ico
-// @version      1.2.13
+// @version      1.2.14
 // @grant        unsafeWindow
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
 // @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/anime-toplist.user.js
