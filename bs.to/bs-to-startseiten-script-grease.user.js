1f42247af6ddb62f9509d0060fecb8b3771f0a23
Sven Liebig <liebigsv@gmail.com>
2016-05-27 13:00:42 +0200

    update url von grease auf den normalen branch

--- bs.to/bs-to-startseiten-script-grease.user.js
+++ bs.to/bs-to-startseiten-script-grease.user.js
@@ -5,9 +5,9 @@
 // @description  Startseiten script für bs.to (unter grease)
 // @include      https://bs.to/
 // @icon         https://s.bs.to/favicon.ico
-// @version      1.0.5
+// @version      1.0.6
 // @require      https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js
-// @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/bs.to/bs-to-startseiten-script-grease.user.js
+// @updateURL    https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/anime-toplist.user.js
 // ==/UserScript==
 
 
