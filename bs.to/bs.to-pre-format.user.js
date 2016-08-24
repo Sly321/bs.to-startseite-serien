a3c11bec4e4f11be3df94fcf91eb103484f3c084
Sven Liebig <liebigsv@gmail.com>
2016-08-24 10:57:00 +0200

    pre format labels for container

--- bs.to/bs.to-pre-format.user.js
+++ bs.to/bs.to-pre-format.user.js
@@ -5,7 +5,7 @@
 // @description  Formatiert die bs.to Startseite für die späteren Designs.
 // @include      https://bs.to/
 // @icon         https://s.bs.to/favicon.ico
-// @version      1.0.1
+// @version      1.1.0
 // @updateURL	 https://raw.githubusercontent.com/Sly321/bs.to-startseite-serien/master/bs.to/bs.to-pre-format.user.js
 // @require		 https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @grant        unsafeWindow
@@ -25,6 +25,7 @@
 	// Start
 	var favoritesLabel = $(document.createElement("label"));
 	favoritesLabel.html("Favoriten ");
+	favoritesLabel.addClass("label-Favoriten");
 	unsafeWindow.favoritesCheckbox = $(document.createElement("input"));
 	favoritesCheckbox.attr("type", "checkbox");
 	favoritesCheckbox.attr("checked", "checked");
@@ -50,5 +51,20 @@
 			buildListElement(leftContainer, this.values, this.title);
 		else
 			buildListElement(rightContainer, this.values, this.title);
+		var checkboxLabel = $(document.createElement("label"));
+		checkboxLabel.html(this.title + " ");
+		var checkbox = $(document.createElement("input"));
+		checkbox.attr("type", "checkbox");
+		checkbox.attr("checked", "checked");
+		var self = this;
+		checkboxLabel.addClass("label-" + this.title);
+		checkbox.on("change", function() {
+			if ($(this).prop("checked"))
+				showList(self.title);
+			else
+				hideList(self.title);
+		});
+		checkboxLabel.append(checkbox);
+		nav.append(checkboxLabel);
 	});
 })();
